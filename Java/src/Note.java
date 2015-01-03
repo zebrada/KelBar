cd408340ba88e43d41c50c18142e236c4ce5b1e2
zebrada <mickael.goualard@gmail.com>
2015-01-03 12:56:00 +0100

    Fini Note et Evenement

--- Java/src/Note.java
+++ Java/src/Note.java
@@ -12,18 +12,64 @@ public class Note
 	
 	private Bar barNote;
 	
-	public Note( Abonne u, int val, String com, Bar b )
+	/**
+	 * Génère une Note
+	 * @param abonneNoteur
+	 * @param note
+	 * @param commentaire
+	 * @param barNote
+	 */
+	
+	public Note( Abonne a, int val, String com, Bar b )
 	{
-		
+		this.abonneNoteur = a;
+		this.note = val;
+		this.commentaire = com;
+		this.barNote = b;
 	}
 	
-	public boolean transfererNote( )
+	public Abonne getAbonneNoteur() {
+		return abonneNoteur;
+	}
+
+	public int getNote() {
+		return note;
+	}
+	
+	public String getCommentaire() {
+		return commentaire;
+	}
+	
+	public Bar getBarNote() {
+		return barNote;
+	}
+	
+	public void setAbonneNoteur(Abonne abonneNoteur) {
+		this.abonneNoteur = abonneNoteur;
+	}
+	
+	public void setNote(int note) {
+		this.note = note;
+	}
+	
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+	
+	public void setBarNote(Bar barNote) {
+		this.barNote = barNote;
+	}
+	
+
+	public boolean transfererNote(Abonne a)
 	{
-		return false;
+		this.abonneNoteur = a;
+		return true;
 	}
 	
 	public boolean supprimerNote( )
 	{
+		//Cette fonction doit se trouver dans le catalogue
 		return false;
 	}
 	
